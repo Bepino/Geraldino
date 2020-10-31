a2a46e1381942b7ba55fc0265b6f26b1811359fa
Bepino <47529128+Bepino@users.noreply.github.com>
2020-10-31 14:41:03 +0100

    Did some maintnance

--- index.js
+++ index.js
@@ -1,129 +1,137 @@
 const config = require('./config.json');
-var DataSet = require('./DataSet.json');
-var Archive = require('./ArchiveSet.json');
 const Discord = require('discord.js');
-const fs = require('fs');
-
 const client = new Discord.Client();
+var SentMessageToday = false;
+var SentMessageTodayZup = false;
 
 client.once('ready', () => {
     console.log(`Ready as ${client.user.username}!`);
-    setInterval(function() {AreWeThere()}, 5 *1000)
-    
+    GetGlobal();
+    GetZupan();
+    //setInterval(function() {GetGlobal(); /*GetZupan()*/}, 60 *1000);
 });
 
 client.login(config.token);
 
-function AreWeThere() {
-    var currdate = Date.now.apply();
-    var jsondate = Date.parse(DataSet.datum);
-    console.log('------------------------------------------\nThe current time difference is: ' 
-                + ((currdate - jsondate)/3600000).toString() + ' hours\n------------------------------------------');
+function GetZupan() {
+    /*
+    repetition protection goes here 
+    */
+    var url = 'https://www.koronavirus.hr/json/?action=po_danima_zupanijama';
 
-    if(currdate - jsondate > 86400000) {
-        ArchiveList();
-    }
-}
-
-function ArchiveList() {
-    SaveJson(JSON.stringify(DataSet), 'ArchiveSet.json');
-    console.log('Archived yesterday data\n---------------------------------------');
-
-    GetList();
-}
-
-function GetList() {
-    console.log('Refreshing list\n---------------------------------------')
+    console.log('Getting list zupanije\n---------------------------------------');
 
     var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
     var xmlhttp = new XMLHttpRequest();
-    var xmlhttpreq2 = new XMLHttpRequest();
-    var url = "https://www.koronavirus.hr/json/?action=podaci_zadnji";
-    var urlz = "https://www.koronavirus.hr/json/?action=po_danima_zupanijama_zadnji";
-    var raw = {
-        zarazeni : 0,
-        datum : "1970-1-1 00:00",
-        novozarazeni : 0,
-        aktivni : 0,
-        Zupanija : {
-            zarazeni : 0,
-            novozarazeni : 0,
-            datum : 0,
-            ime : 0,
-            morto : 0,
-            aktiv : 0
-        }
-    };
-
-    var goAhead = false; 
-    var gOAhead = false;
 
     xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        console.log(this.responseText + '\n---------------------------------------')
-        var data = JSON.parse(this.responseText);
-        raw.zarazeni = data[0].SlucajeviHrvatska;
-        raw.novozarazeni = data[0].SlucajeviHrvatska - Archive.zarazeni;
-        raw.datum = data[0].Datum;
-        let morto = data[0].UmrliHrvatska + data[0].IzlijeceniHrvatska;
-        raw.aktivni = data[0].SlucajeviHrvatska - morto;
+        //console.log(this.responseText + '\n---------------------------------------');
+        let response = JSON.parse(this.responseText);
+        var data = [{
+            Datum : response[0].Datum,
+            Aktivni : response[0].PodaciDetaljno[19].broj_aktivni,
+            Umrli : response[0].PodaciDetaljno[19].broj_umrlih,
+            Zarazeni : response[0].PodaciDetaljno[19].broj_zarazenih
+        },{
+            Aktivni : response[1].PodaciDetaljno[19].broj_aktivni,
+            Umrli : response[1].PodaciDetaljno[19].broj_umrlih,
+            Zarazeni : response[1].PodaciDetaljno[19].broj_zarazenih
+        },{
+            Zarazeni : response[2].PodaciDetaljno[19].broj_zarazenih
+        }];
 
-        goAhead = true;
-        }
-    }; 
-
-    xmlhttpreq2.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        console.log(this.responseText + '\n---------------------------------------')
-        var data = JSON.parse(this.responseText);
-        raw.Zupanija.datum = data[0].Datum;
-        raw.Zupanija.ime = data[0].PodaciDetaljno[19].Zupanija;
-        raw.Zupanija.zarazeni = data[0].PodaciDetaljno[19].broj_zarazenih;
-        raw.Zupanija.novozarazeni = data[0].PodaciDetaljno[19].broj_zarazenih - Archive.Zupanija.zarazeni;
-        raw.Zupanija.morto = data[0].PodaciDetaljno[19].broj_umrlih;
-        raw.Zupanija.aktiv = data[0].PodaciDetaljno[19].broj_aktivni;
-
-        gOAhead = true;
+        console.log('Response data zupanije: \n' + data + '\n---------------------------------------');
+        
+        SendBigMessage(false, data);
         }
     }; 
 
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
+}
 
-    xmlhttpreq2.open("GET", urlz, true);
-    xmlhttpreq2.send();
+function GetGlobal() {
+    /*
+    repetition protection goes here 
+    */
 
-    var NotDone = true;
-    setInterval( function() {
-        if(goAhead && gOAhead && NotDone) {
-            SaveJson(JSON.stringify(raw), 'DataSet.json').then( () => {
-                SendBigMessage();
-                NotDone = false;
-            
-            
-            });
+    var url = 'https://www.koronavirus.hr/json/?action=podaci';
+
+    console.log('Getting list\n---------------------------------------');
+
+    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        //console.log(this.responseText + '\n---------------------------------------');
+        let response = JSON.parse(this.responseText);
+        var data = [{
+            SlucajeviHrvatska : response[0].SlucajeviHrvatska, 
+            UmrliHrvatska : response[0].UmrliHrvatska,
+            IzlijeceniHrvatska : response[0].IzlijeceniHrvatska,
+            Datum : response[0].Datum
+            }, {
+            SlucajeviHrvatska : response[1].SlucajeviHrvatska, 
+            UmrliHrvatska : response[1].UmrliHrvatska,
+            IzlijeceniHrvatska : response[1].IzlijeceniHrvatska,
+            }, {
+                SlucajeviHrvatska : response[2].SlucajeviHrvatska, 
+            }];
+
+        console.log('Response data: \n' + data + '\n---------------------------------------');
+        
+        SendBigMessage(true, data)
         }
-    }, 500);
+    }; 
+
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
 }
 
-function SendBigMessage() {
-    var DataSet = JSON.parse(fs.readFileSync('DataSet.json', {encoding:'utf-8'}));
-    var Archive = JSON.parse(fs.readFileSync('ArchiveSet.json', {encoding:'utf-8'}));
-    let the_message = `** Narodne Novine: Izdanje ${DataSet.datum.split(" ")[0]}**\n\n**Hrvatska:**\nAktivni: ${DataSet.aktivni}\n`+
-        `Novozarazeni: ${DataSet.novozarazeni}(${DataSet.novozarazeni - Archive.novozarazeni})` +
-        `\n\n**Zadarska županija:**\nNovozarazeni: ${DataSet.Zupanija.novozarazeni}(${DataSet.Zupanija.novozarazeni - Archive.Zupanija.novozarazeni})`+
-        `\nAktivni: ${DataSet.Zupanija.aktiv}`;
-    //console.log(the_message);
-    client.channels.cache.get('459666776054169602').send(the_message);
-}
+function SendBigMessage(flag, data) {
+    var send = 'Something broke with the machine: 500 (idk)';
+    if(flag) {
+        var morto = data[0].SlucajeviHrvatska - data[0].IzlijeceniHrvatska - data[0].UmrliHrvatska;
 
-function SaveJson(data, filename) {
-    return new Promise(resolve => {
-        fs.writeFile(filename, data, function (err) {
-            if(err) throw err;
-            console.log('Saved JSON file');
-            console.log("Promise (SaveJson -> this.caller): Done\n---------------------------------------");
-            resolve();
-        })
-    });
+        var diffAktiv = morto - (data[1].SlucajeviHrvatska - data[1].IzlijeceniHrvatska - data[1].UmrliHrvatska);
+        if(diffAktiv > 0)
+            diffAktiv = '+' + diffAktiv.toString();
+
+        var diffnovo = (data[0].SlucajeviHrvatska - data[1].SlucajeviHrvatska) - (data[1].SlucajeviHrvatska - data[2].SlucajeviHrvatska);
+        if(diffnovo > 0)
+            difnovo = '+' + difnovo.toString();
+
+        var diffumrli = data[0].UmrliHrvatska - data[1].UmrliHrvatska;
+        if(diffumrli > 0)
+            diffumrli = '+' + diffumrli.toString();
+
+        var Aktivni = `Aktivni : ${morto} (${diffAktiv})`;
+        var Novozarazeni = `Novozarazeni : ${data[0].SlucajeviHrvatska - data[1].SlucajeviHrvatska} (${diffnovo})`;
+        var umrli = `Umrli : ${data[0].UmrliHrvatska} (${diffumrli})`;
+        send = `**Narodne Novine izdanje : ${data[0].Datum}**\n${Novozarazeni}\n${Aktivni}\n${umrli}`;
+    }
+    else {
+        let diffAktiv = data[0].Aktivni - data[1].Aktivni;
+        if(diffAktiv > 0)
+            diffAktiv = '+' + diffAktiv.toString();
+
+        let diffNovo = (data[0].Zarazeni - data[1].Zarazeni) - (data[1].Zarazeni - data[2].Zarazeni);
+        if(diffNovo > 0)
+            diffNovo = '+' + diffNovo.toString();
+
+        let diffUmrli = data[0].Umrli - data[1].Umrli;
+        if(diffUmrli > 0)
+            diffUmrli = '+' + diffUmrli.toString();
+
+        var Umrli = `Umrli : ${data[0].Umrli} (${diffUmrli})`;
+        var Novozarazeni = `Novozarazeni : ${data[0].Zarazeni - data[1].Zarazeni} (${diffNovo})`;        
+        var Aktivni = `Aktivni : ${data[0].Aktivni} (${diffAktiv})`;
+        send = `**Županijske novine izdanje : ${data[0].Datum}**\n${Aktivni}\n${Novozarazeni}\n${Umrli}`;
+    }
+
+    console.log(send + '\n---------------------------------------');
+    //client.channels.cache.get('459666776054169602').send(send);
+    console.log('Sent message\n---------------------------------------');
 }
