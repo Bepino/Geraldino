a8a5fd8f35dd47463e2b4aef609e0a96cd834372
Bepino <47529128+Bepino@users.noreply.github.com>
2021-02-18 21:21:46 +0100

    lets see if this fixes it

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const discord_token = process.env.discord_token;
 var Discord = require("discord.js");
 var client = new Discord.Client();
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const fetch = require('node-fetch');
 
 //Putting these vars here so they can be reset monthly to avoid -30 timespans
 var LastZupSent = new Date();  //Zup var
@@ -17,7 +17,7 @@ client.once('ready', () =>{
     setInterval(function(){GetGlobal(); GetZupan()}, 60*1000);  // 1 minute interval
 });
 
-console.log(discord_token);
+//console.log(discord_token);
 client.login(discord_token);
 
 //Test command
@@ -32,6 +32,32 @@ function GetZupan(){
 
     console.log('(Zupanija )Getting list\n---------------------------------------');
 
+    fetch(url)
+        .then(rsp => rsp.json())
+        .then((json) => {
+            const data = [
+                {
+                    "Zarazeni" : json[0].PodaciDetaljno[19].broj_zarazenih,
+                    "Aktivni" : json[0].PodaciDetaljno[19].broj_aktivni,
+                    "Umrli" : json[0].PodaciDetaljno[19].broj_umrlih,
+                    "Datum" : json[0].Datum
+                },
+                {
+                    "Zarazeni" : json[1].PodaciDetaljno[19].broj_zarazenih,
+                    "Aktivni" : json[1].PodaciDetaljno[19].broj_aktivni,
+                    "Umrli" : json[1].PodaciDetaljno[19].broj_umrlih,
+                    "Datum" : json[1].Datum
+                },
+                {
+                    "Zarazeni" : json[2].PodaciDetaljno[19].broj_zarazenih,
+                    "Aktivni" : json[2].PodaciDetaljno[19].broj_aktivni,
+                    "Umrli" : json[2].PodaciDetaljno[19].broj_umrlih,
+                    "Datum" : json[2].Datum
+                }
+            ];
+            SendBigMessage(true, data)
+        }
+    );
 }
 
 function GetGlobal(){
@@ -39,6 +65,32 @@ function GetGlobal(){
 
     console.log('(Hrvatska) Getting list\n---------------------------------------');
 
+    fetch(url)
+        .then(rsp => rsp.json())
+        .then((json) => {
+            const data = [
+                {
+                    "SlucajeviHrvatska" : json[0].SlucajeviHrvatska,
+                    "IzlijeceniHrvatska" : json[0].IzlijeceniHrvatska,
+                    "UmrliHrvatska" : json[0].UmrliHrvatska,
+                    "Datum" : json[0].Datum
+                },
+                {
+                    "SlucajeviHrvatska" : json[1].SlucajeviHrvatska,
+                    "IzlijeceniHrvatska" : json[1].IzlijeceniHrvatska,
+                    "UmrliHrvatska" : json[1].UmrliHrvatska,
+                    "Datum" : json[1].Datum
+                },
+                {
+                    "SlucajeviHrvatska" : json[2].SlucajeviHrvatska,
+                    "IzlijeceniHrvatska" : json[2].IzlijeceniHrvatska,
+                    "UmrliHrvatska" : json[2].UmrliHrvatska,
+                    "Datum" : json[2].Datum
+                }
+            ];
+            SendBigMessage(false, data)
+        }
+    );
 }
 
 function SendBigMessage(flag, data){
@@ -89,7 +141,8 @@ function SendBigMessage(flag, data){
     var identifier = '(Zupanija)';
     if(flag)
         identifier = '(Hrvatska)';
-    //console.log(send + '\n---------------------------------------');
+
+    console.log(send + '\n---------------------------------------');
     client.channels.cache.get('459666776054169602').send(send);
-    console.log(identifier + 'Sent message\n---------------------------------------');
+    console.log(identifier + ' Sent message\n---------------------------------------');
     }
